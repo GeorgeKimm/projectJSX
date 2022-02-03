93fedeefe7ce940046ecbc56d83f68ebdca8207c
george <g.kim@sergek.kz>
2022-02-03 10:45:16 +0600

    drag and drop

--- src/components/listPolygon/elementPolygon.jsx
+++ src/components/listPolygon/elementPolygon.jsx
@@ -2,7 +2,6 @@ import React, { useState } from "react";
 import NavigateNextIcon from "@material-ui/icons/NavigateNext";
 import ExpandMore from "@material-ui/icons/ExpandMore";
 import RoomIcon from "@material-ui/icons/Room";
-import { Button, Menu, MenuItem } from "@material-ui/core";
 import {
   Typography,
   Box,
@@ -10,8 +9,10 @@ import {
   ListItem,
   ListItemText,
   Collapse,
+  Button,
 } from "@material-ui/core";
 import makeStyles from "@material-ui/core/styles/makeStyles";
+import { Draggable } from "react-beautiful-dnd";
 
 import { CameraPolygon } from "./cameraPolygon";
 import shorten from "./shorten";
@@ -49,33 +50,266 @@ export function ElementPolygon(props) {
   console.log("cameras", cameras);
   return (
     <>
-      <List component="div" className={classes.root}>
-        <ListItem button onClick={clickList}>
-          <Box className={classes.box}>
-            <Typography color="textSecondary" variant="h6">
-              {index + 1}
-            </Typography>
-          </Box>
-          <Box className={classes.box}>
-            {open ? (
-              <ExpandMore color="action" />
-            ) : (
-              <NavigateNextIcon color="action" />
-            )}
-          </Box>
-          <Box className={classes.box}>
-            <RoomIcon color="primary" />
-          </Box>
-          <ListItemText primary={shortTitle} className={classes.box} />
-        </ListItem>
-        <Collapse in={open} timeout="auto">
-          <List component="div" disablePadding>
-            {cameras.map((camera) => {
-              return <CameraPolygon key={camera.id} camera={camera} />;
-            })}
+      <Draggable draggableId={`${location.id}`} index={index}>
+        {(provided) => (
+          <List
+            {...provided.draggableProps}
+            {...provided.dragHandleProps}
+            ref={provided.innerRef}
+            component="div"
+            className={classes.root}
+          >
+            <ListItem>
+              <Box className={classes.box}>
+                <Typography color="textSecondary" variant="h6">
+                  {index + 1}
+                </Typography>
+              </Box>
+              <Box className={classes.box}>
+                {open ? (
+                  <Button onClick={clickList}>
+                    <ExpandMore color="action" />
+                  </Button>
+                ) : (
+                  <Button onClick={clickList}>
+                    <NavigateNextIcon color="action" />
+                  </Button>
+                )}
+              </Box>
+              <Box className={classes.box}>
+                <RoomIcon color="primary" />
+              </Box>
+              <ListItemText primary={shortTitle} className={classes.box} />
+            </ListItem>
+            <Collapse in={open} timeout="auto">
+              <List component="div" disablePadding>
+                {cameras.map((camera) => {
+                  return <CameraPolygon key={camera.id} camera={camera} />;
+                })}
+              </List>
+            </Collapse>
           </List>
-        </Collapse>
-      </List>
+        )}
+      </Draggable>
     </>
   );
 }
+
+// main
+
+// import React, { useState } from "react";
+// import NavigateNextIcon from "@material-ui/icons/NavigateNext";
+// import ExpandMore from "@material-ui/icons/ExpandMore";
+// import RoomIcon from "@material-ui/icons/Room";
+// import {
+//   Typography,
+//   Box,
+//   List,
+//   ListItem,
+//   ListItemText,
+//   Collapse,
+//   Button,
+// } from "@material-ui/core";
+// import makeStyles from "@material-ui/core/styles/makeStyles";
+
+// import { CameraPolygon } from "./cameraPolygon";
+// import shorten from "./shorten";
+
+// const useStyles = makeStyles({
+//   root: {
+//     padding: 0,
+//     borderBottom: "1px solid #C4C4C4",
+//   },
+//   box: {
+//     display: "flex",
+//     justifyContent: "center",
+//     alignItems: "center",
+//     padding: "5px",
+//   },
+//   icon: {
+//     width: "12px",
+//     height: "16px",
+//   },
+// });
+
+// export function ElementPolygon(props) {
+//   const [open, setOpen] = useState(false);
+//   const classes = useStyles();
+
+//   const { location, index } = props;
+//   const { title, cameras } = location;
+
+//   const shortTitle = shorten(title);
+
+//   function clickList() {
+//     setOpen(!open);
+//   }
+
+//   console.log("cameras", cameras);
+//   return (
+//     <>
+//       <List component="div" className={classes.root}>
+//         <ListItem>
+//           <Box className={classes.box}>
+//             <Typography color="textSecondary" variant="h6">
+//               {index + 1}
+//             </Typography>
+//           </Box>
+//           <Box className={classes.box}>
+//             {open ? (
+//               <Button onClick={clickList}>
+//                 <ExpandMore color="action" />
+//               </Button>
+//             ) : (
+//               <Button onClick={clickList}>
+//                 <NavigateNextIcon color="action" />
+//               </Button>
+//             )}
+//           </Box>
+//           <Box className={classes.box}>
+//             <RoomIcon color="primary" />
+//           </Box>
+//           <ListItemText primary={shortTitle} className={classes.box} />
+//         </ListItem>
+//         <Collapse in={open} timeout="auto">
+//           <List component="div" disablePadding>
+//             {cameras.map((camera) => {
+//               return <CameraPolygon key={camera.id} camera={camera} />;
+//             })}
+//           </List>
+//         </Collapse>
+//       </List>
+//     </>
+//   );
+// }
+
+// test1
+
+// import React, { useState } from "react";
+// import NavigateNextIcon from "@material-ui/icons/NavigateNext";
+// import ExpandMore from "@material-ui/icons/ExpandMore";
+// import RoomIcon from "@material-ui/icons/Room";
+// import {
+//   Typography,
+//   Box,
+//   List,
+//   ListItem,
+//   ListItemText,
+//   Collapse,
+//   Button,
+// } from "@material-ui/core";
+// import makeStyles from "@material-ui/core/styles/makeStyles";
+// import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
+
+// import { CameraPolygon } from "./cameraPolygon";
+// import shorten from "./shorten";
+
+// const useStyles = makeStyles({
+//   root: {
+//     padding: 0,
+//     borderBottom: "1px solid #C4C4C4",
+//   },
+//   box: {
+//     display: "flex",
+//     justifyContent: "center",
+//     alignItems: "center",
+//     padding: "5px",
+//   },
+//   icon: {
+//     width: "12px",
+//     height: "16px",
+//   },
+// });
+
+// export function ElementPolygon({ location: locationPolygon, index }) {
+//   const [open, setOpen] = useState(false);
+//   const [location, setLocation] = useState(locationPolygon);
+//   const classes = useStyles();
+
+//   const { title, cameras } = location;
+
+//   const shortTitle = shorten(title);
+
+//   function clickList() {
+//     setOpen(!open);
+//   }
+
+//   function handleOnDragEnd(result) {
+//     if (!result.destination) return;
+
+//     console.log("outttttttttttttttttttt");
+//     const items = Array.from(location);
+//     const [reorderedItem] = items.splice(result.source.index, 1);
+//     items.splice(result.destination.index, 0, reorderedItem);
+
+//     setLocation(items);
+//   }
+
+//   console.log("cameras", cameras);
+//   return (
+//     <>
+//       <DragDropContext onDragEnd={handleOnDragEnd}>
+//         <Droppable droppableId="location">
+//           {(provided) => (
+//             <List
+//               {...provided.droppableProps}
+//               ref={provided.innerRef}
+//               component="div"
+//               className={classes.root}
+//             >
+//               <Draggable
+//                 key={location.id}
+//                 draggableId={location.id}
+//                 index={index}
+//               >
+//                 {(provided) => (
+//                   <>
+//                     <ListItem
+//                       {...provided.draggableProps}
+//                       {...provided.dragHandleProps}
+//                       ref={provided.innerRef}
+//                     >
+//                       <Box className={classes.box}>
+//                         <Typography color="textSecondary" variant="h6">
+//                           {index + 1}
+//                         </Typography>
+//                       </Box>
+//                       <Box className={classes.box}>
+//                         {open ? (
+//                           <Button onClick={clickList}>
+//                             <ExpandMore color="action" />
+//                           </Button>
+//                         ) : (
+//                           <Button onClick={clickList}>
+//                             <NavigateNextIcon color="action" />
+//                           </Button>
+//                         )}
+//                       </Box>
+//                       <Box className={classes.box}>
+//                         <RoomIcon color="primary" />
+//                       </Box>
+//                       <ListItemText
+//                         primary={shortTitle}
+//                         className={classes.box}
+//                       />
+//                     </ListItem>
+//                     <Collapse in={open} timeout="auto">
+//                       <List component="div" disablePadding>
+//                         {cameras.map((camera) => {
+//                           return (
+//                             <CameraPolygon key={camera.id} camera={camera} />
+//                           );
+//                         })}
+//                       </List>
+//                     </Collapse>
+//                   </>
+//                 )}
+//               </Draggable>
+//               {provided.placeholder}
+//             </List>
+//           )}
+//         </Droppable>
+//       </DragDropContext>
+//     </>
+//   );
+// }
--- src/components/listPolygon/listPolygon.jsx
+++ src/components/listPolygon/listPolygon.jsx
@@ -1,30 +1,122 @@
 import React, { useState } from "react";
-import makeStyles from "@material-ui/core/styles/makeStyles";
+import { Box } from "@material-ui/core";
+import { DragDropContext, Droppable } from "react-beautiful-dnd";
 
 import { ElementPolygon } from "./elementPolygon";
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    flexGrow: 1,
-  },
-}));
-
 export function ListPolygon({ data }) {
-  const { locations } = data;
-  const classes = useStyles();
+  const locationsPolygon = data.locations;
+
+  const [locations, SetLocations] = useState(locationsPolygon);
+
+  function handleOnDragEnd(result) {
+    if (!result.destination) return;
+
+    console.log("outttttttttttttttttttt");
+    const items = Array.from(locations);
+    const [reorderedItem] = items.splice(result.source.index, 1);
+    items.splice(result.destination.index, 0, reorderedItem);
+
+    SetLocations(items);
+  }
 
   return (
-    <>
-      {locations.map((location, index) => {
-        return (
-          <ElementPolygon
-            className={classes.root}
-            key={location.id}
-            location={location}
-            index={index}
-          />
-        );
-      })}
-    </>
+    <DragDropContext onDragEnd={handleOnDragEnd}>
+      <Droppable droppableId="locations">
+        {(provided) => (
+          <Box {...provided.droppableProps} ref={provided.innerRef}>
+            {locations.map((location, index) => {
+              return (
+                <ElementPolygon
+                  key={location.id}
+                  location={location}
+                  index={index}
+                />
+              );
+            })}
+            {provided.placeholder}
+          </Box>
+        )}
+      </Droppable>
+    </DragDropContext>
   );
 }
+
+// main
+
+// export function ListPolygon({ data }) {
+//   const { locations } = data;
+
+//   return (
+//     <>
+//       {locations.map((location, index) => {
+//         return (
+//           <ElementPolygon key={location.id} location={location} index={index} />
+//         );
+//       })}
+//     </>
+//   );
+// }
+
+// all here
+
+// import React, { useState } from "react";
+// import { Box } from "@material-ui/core";
+// import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
+
+// import { ElementPolygon } from "./elementPolygon";
+
+// export function ListPolygon({ data }) {
+//   // console.log("data", data);
+//   // console.log("data.locations", data.locations);
+
+//   const locationsPolygon = data.locations;
+
+//   // console.log("locationsPolygon", locationsPolygon);
+//   const [locations, SetLocations] = useState(locationsPolygon);
+//   // console.log("locations", locations);
+
+//   function handleOnDragEnd(result) {
+//     if (!result.destination) return;
+
+//     console.log("outttttttttttttttttttt");
+//     const items = Array.from(locations);
+//     const [reorderedItem] = items.splice(result.source.index, 1);
+//     items.splice(result.destination.index, 0, reorderedItem);
+
+//     SetLocations(items);
+//   }
+
+//   return (
+//     <>
+//       <DragDropContext onDragEnd={handleOnDragEnd}>
+//         <Droppable droppableId="locations">
+//           {(provided) => (
+//             <Box {...provided.droppableProps} ref={provided.innerRef}>
+//               {locations.map((location, index) => {
+//                 return (
+//                   <Draggable
+//                     key={location.id}
+//                     draggableId={location.id}
+//                     index={index}
+//                   >
+//                     {(provided) => (
+//                       <Box
+//                         {...provided.draggableProps}
+//                         {...provided.dragHandleProps}
+//                         ref={provided.innerRef}
+//                       >
+//                         <ElementPolygon location={location} index={index} />
+//                       </Box>
+//                     )}
+//                   </Draggable>
+//                 );
+//               })}
+//               {provided.placeholder}
+//             </Box>
+//           )}
+//         </Droppable>
+//       </DragDropContext>
+//     </>
+//   );
+// }
