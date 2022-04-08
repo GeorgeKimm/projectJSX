5c5a61a54a73e384a2c7eafef010951bb8f1b88b
george <g.kim@sergek.kz>
2022-04-08 17:44:16 +0600

    drag and drop

--- src/components/listPolygon/listsPolygons.jsx
+++ src/components/listPolygon/listsPolygons.jsx
@@ -1,21 +1,44 @@
 import React from "react";
-import { Grid, Typography } from "@material-ui/core";
+import { Grid, Typography, Box } from "@material-ui/core";
 import makeStyles from "@material-ui/core/styles/makeStyles";
-
 import { ListPolygon } from "./listPolygon";
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    maxWidth: "450px",
+    display: "flex",
+    justifyContent: "space-around",
+  },
+  container: {
+    height: "82vh",
+    overflowY: "auto",
+    "&::-webkit-scrollbar": {
+      width: "6px",
+    },
+    "&::-webkit-scrollbar-track": {
+      backgroundColor: "#F0F0F0",
+    },
+    "&::-webkit-scrollbar-thumb": {
+      backgroundColor: "#50A7D9",
+      borderRadius: "4px",
+    },
+    "&::-webkit-scrollbar-thumb:hover": {
+      backgroundColor: "#427c9d",
+    },
+    scrollbarWidth: "auto",
+    scrollbarColor: "#50A7D9 #F0F0F0",
   },
   calibrator: {
     display: "flex",
     flexGrow: 1,
     flexWrap: "nowrap",
-    padding: "10px",
+    padding: theme.spacing(1),
     color: "#ffffff",
     background: "rgba(80, 167, 217, 0.6)",
     borderRadius: "4px",
+  },
+  box: {
+    justifyContent: "space-between",
+    flexWrap: "wrap",
   },
   numCalibrator: {
     fontWeight: "700",
@@ -37,28 +60,35 @@ const useStyles = makeStyles((theme) => ({
 export function ListsPolygons({ arr }) {
   const classes = useStyles();
 
-  return arr.map((item) => {
-    const { id, calibrator, speedTestUser } = item;
-    return (
-      <Grid container direction="column" key={id} className={classes.root}>
-        <Grid item className={classes.calibrator}>
-          <Grid container spacing={2}>
-            <Grid item>
-              <Typography className={classes.numCalibrator}>
-                {speedTestUser.carNumber}
-              </Typography>
+  return (
+    <Box className={classes.root}>
+      <Box style={{ width: "200px", height: "200px", padding: 20 }}>hello</Box>
+      <Box className={classes.container}>
+        {arr.map((item) => {
+          const { id, calibrator, speedTestUser } = item;
+          return (
+            <Grid container direction="column" key={id}>
+              <Grid item className={classes.calibrator}>
+                <Grid container spacing={1} className={classes.box}>
+                  <Grid item>
+                    <Typography className={classes.numCalibrator}>
+                      {speedTestUser.carNumber}
+                    </Typography>
+                  </Grid>
+                  <Grid item>
+                    <Typography className={classes.nameCalibrator}>
+                      {calibrator.fullName}
+                    </Typography>
+                  </Grid>
+                </Grid>
+              </Grid>
+              <Grid item>
+                <ListPolygon className={classes.listPolygon} data={item} />
+              </Grid>
             </Grid>
-            <Grid item>
-              <Typography className={classes.nameCalibrator}>
-                {calibrator.fullName}
-              </Typography>
-            </Grid>
-          </Grid>
-        </Grid>
-        <Grid item>
-          <ListPolygon className={classes.listPolygon} data={item} />
-        </Grid>
-      </Grid>
-    );
-  });
+          );
+        })}
+      </Box>
+    </Box>
+  );
 }
